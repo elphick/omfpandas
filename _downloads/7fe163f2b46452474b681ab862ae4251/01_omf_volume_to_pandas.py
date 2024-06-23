57f750589551719bc162dfd1ac6e9cd01262793d
elphick <elphick@users.noreply.github.com>
2024-06-23 12:21:57 +0000

    deploy: 883c91f5db45cb9369a7725d71f87ea10c8bdfb9

---
+++ _downloads/7fe163f2b46452474b681ab862ae4251/01_omf_volume_to_pandas.py
@@ -0,0 +1,40 @@
+"""
+OMF Block Model to DataFrame
+============================
+
+An omf VolumeElement represents a `Block Model`, and can be converted to a Pandas DataFrame.
+
+"""
+from pathlib import Path
+
+import pandas as pd
+
+from omfpandas import OMFPandasReader
+
+# %%
+# Instantiate
+# -----------
+# Create the object OMFPandas with the path to the OMF file.
+test_omf_path: Path = Path('./../assets/test_file.omf')
+omfp: OMFPandasReader = OMFPandasReader(filepath=test_omf_path)
+
+# %%
+# Convert
+# -------
+# We'll inspect the elements in the omf file, and determine what volume element to convert.
+
+omfp.elements
+
+# %%
+# We can see by inspection that we have one volume element in the omf file called 'Block Model, so we will
+# convert that to a Pandas DataFrame.
+
+blocks: pd.DataFrame = omfp.read_volume(volume_name='Block Model', variables=None, with_geometry_index=True)
+print(f"DataFrame shape: {blocks.shape}")
+blocks.head()
+
+# %%
+# The index contains the centroid coordinates and the dimensions of the block.
+# The columns contain the variables in the block model, though only variables assigned to the `cell`
+# (as distinct from the grid `points`) are loaded.
+
