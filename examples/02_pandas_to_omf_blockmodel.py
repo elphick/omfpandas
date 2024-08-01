804a1c4077ce9988a7e666779dbcece97a2cdba8
Greg <11791585+elphick@users.noreply.github.com>
2024-08-01 19:59:53 +0800

    managed empty values for integers using sentinel value

--- examples/02_pandas_to_omf_blockmodel.py
+++ examples/02_pandas_to_omf_blockmodel.py
@@ -28,13 +28,36 @@ blocks: pd.DataFrame = OMFPandasReader(filepath=test_omf_filepath).read_blockmod
 blocks.head()
 
 # %%
-# Write a new block model
-# -----------------------
+# Modify the BlockModel
+# ---------------------
 #
-# We'll modify the block model and write it back to another OMF file.
+# We'll modify the block model using Pandas and write it back to another OMF file.
+# Add a new float column to the block model.
 
 blocks['density'] = 2.7
-blocks['porosity'] = 0.1
+# set the second density record to nan
+blocks.iloc[1, blocks.columns.get_loc('density')] = np.nan
+
+# %%
+# Create an integer variable
+
+np.random.seed(42)
+blocks['some_int'] = pd.Series(np.random.choice([0, 1, 2, np.nan], size=blocks.shape[0], ), dtype='Int8',
+                               index=blocks.index)
+
+# %%
+# Create a categorical variable
+
+blocks['rock_class'] = np.random.choice(['A', 'B', 'C', np.nan], size=blocks.shape[0])
+blocks['rock_class'] = blocks['rock_class'].astype('category')
+blocks.head()
+
+# %%
+blocks.dtypes
+
+# %%
+# Write to OMF
+# ------------
 
 new_omf_filepath: Path = Path('modified_test_file.omf')
 
@@ -50,6 +73,9 @@ saved_blocks: pd.DataFrame = OMFPandasReader(filepath=new_omf_filepath).read_blo
 saved_blocks.head()
 
 # %%
+saved_blocks.dtypes
+
+# %%
 # Validation
 # ----------
 # Load the two omf projects and compare the CU_pct data for each
@@ -58,7 +84,8 @@ project_0: Project = omf.load(str(test_omf_filepath))
 project_1: Project = omf.load(str(new_omf_filepath))
 
 bm_1: omf.TensorGridBlockModel = [element for element in project_0.elements if element.name == 'vol'][0]
-bm_2: omf.TensorGridBlockModel = [element for element in project_1.elements if element.name == 'Modified Block Model'][0]
+bm_2: omf.TensorGridBlockModel = [element for element in project_1.elements if element.name == 'Modified Block Model'][
+    0]
 
 bm_cu_1 = bm_1.attributes[0]
 bm_cu_2 = bm_2.attributes[0]
