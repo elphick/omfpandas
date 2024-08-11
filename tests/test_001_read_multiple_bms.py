1c9aee006fe4ad2742a519af7d323f2a0c031a98
Greg Elphick <11791585+elphick@users.noreply.github.com>
2024-08-11 09:47:46 +0800

    0.6.6 added query to read_block_models.  Added changelog using towncrier. (#24)

--- tests/test_001_read_multiple_bms.py
+++ tests/test_001_read_multiple_bms.py
@@ -1,10 +1,11 @@
+import sys
 import tempfile
+import time
 
 import numpy as np
 import omf
 import pandas as pd
 from pathlib import Path
-
 import pytest
 from omf import Project, NumericAttribute, TensorGridBlockModel
 
@@ -112,4 +113,49 @@ def test_read_block_models():
     assert all(col in df.columns for col in expected_columns)
 
     # Clean up temporary file
+    temp_omf_path.unlink()
+
+
+def measure_execution_time(reader, blockmodel_attributes, query=None, post_query=None):
+    start_time = time.time()
+    df = reader.read_block_models(blockmodel_attributes, query=query)
+    if post_query:
+        df = df.query(post_query)
+    execution_time = time.time() - start_time
+    return df, execution_time
+
+
+def test_read_block_models_with_query():
+    temp_omf_path = create_congruent_omf_file()
+    reader = OMFPandasReader(temp_omf_path)
+
+    blockmodel_attributes = {
+        'BlockModel1': None,
+        'BlockModel2': ['attr4']
+    }
+
+    num_iterations = 10
+    execution_times_df1 = []
+    execution_times_df2 = []
+
+    for _ in range(num_iterations):
+        df1, execution_time_df1 = measure_execution_time(reader, blockmodel_attributes, post_query='attr4 > 0.5')
+        execution_times_df1.append(execution_time_df1)
+
+        df2, execution_time_df2 = measure_execution_time(reader, blockmodel_attributes, query='attr4 > 0.5')
+        execution_times_df2.append(execution_time_df2)
+
+    avg_execution_time_df1 = sum(execution_times_df1) / num_iterations
+    avg_execution_time_df2 = sum(execution_times_df2) / num_iterations
+
+    relative_execution_time = avg_execution_time_df2 / avg_execution_time_df1 if avg_execution_time_df1 != 0 else float(
+        'inf')
+
+    sys.stdout.write(f"\nAverage execution time for df1: {avg_execution_time_df1:.6f} seconds\n")
+    sys.stdout.write(f"Average execution time for df2: {avg_execution_time_df2:.6f} seconds\n")
+    sys.stdout.write(f"Relative execution time (df2/df1): {relative_execution_time:.2f}\n")
+
+    # performance is poorer, but memory is lower
+    # assert relative_execution_time < 1.0
+
     temp_omf_path.unlink()
